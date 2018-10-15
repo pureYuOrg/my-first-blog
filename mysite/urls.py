89c8638300ace554de8944ae1fe64df24c4c60e4
pure_yu <yuliya.s.belovol@gmail.com>
2018-10-15 22:35:55 +0300

    Authorizing added, publishing, draft list added

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#from django.urls import include, path
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import include, path
+
+from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r'', include('blog.urls')),
-#    path('^$', Home.as_view(), name='homepage.html'),
+    #path(r'admin/', admin.site.urls),
+    #path(r'', include('blog.urls')),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
+
+    #    path('^$', Home.as_view(), name='homepage.html'),
 #    path('^$', TemplateView.as_view(template_name='homepage.html')),
 ]
